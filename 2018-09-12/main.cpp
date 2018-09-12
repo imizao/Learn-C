7e7e63ca6c6633ccb0837bec3f4bb69e3f1abf62
apple <2313466338@qq.com>
2018-09-12 10:23:48 +0800

    2018-09-12

---
+++ 2018-09-12/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int var = 20;
+  int *ip = NULL;
+
+  cout << "Value of &var variable:";
+  cout << &var << endl;
+
+  // cout << "Address stored in *ip variable:";
+  // cout << *ip << endl;
+
+  ip = &var;
+
+  cout << "Value of var variable:";
+  cout << var << endl;
+
+  cout << "Address stored in ip variable:";
+  cout << ip << endl;
+
+  cout << "Value of *ip variable:";
+  cout << *ip << endl;
+
+  return 0;
+}
