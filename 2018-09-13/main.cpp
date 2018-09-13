7a0d872b2dfd390037d7db5c6fd83822f5075f13
apple <2313466338@qq.com>
2018-09-13 23:48:52 +0800

    2018-09-13

---
+++ 2018-09-13/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int teledeli = 5;
+  {
+    cout << "Hello\n";
+    int websight = -2;
+    cout << websight << ' ' << teledeli << endl;
+  }
+  cout << teledeli << endl;
+
+  return 0;
+}
