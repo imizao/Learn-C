aa91b0ba3a8b46addffb43b8ad89c85b716da208
apple <2313466338@qq.com>
2018-09-16 23:58:18 +0800

    2018-09-16

--- 2018-09-16/usestock.cpp
+++ 2018-09-16/usestock.cpp
@@ -10,6 +10,18 @@ int mian() {
 
     Stock stock2 = Stock("Boffo objects", 2 ,2.0);
     stock2.show();
+
+    cout << "Assigning stock1 to stock2:\n";
+    stock2 = stock1;
+    cout << "Listing stock1 and stock2:\n";
+    stock1.show();
+    stock2.show();
+
+    cout << "Using a constructor to reset an object\n";
+    stock1 = Stock("Nifty Foods", 10, 50.0);
+    cout << "Revised stock1:\n";
+    stock1.show();
+    cout << "Done\n";
   }
 
   return 0;
